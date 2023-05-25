e56e2517fb72c9f02166cf9a4c09048d73079ee8
PashaNykon <pavlonykon@ukr.net>
2023-05-25 13:42:43 +0300

    111

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+// console.log('Number of categories:', categoriesEl.length);
+// const categoriesNameEl = document.querySelector('h2');
+// console.log('Category:', categoriesNameEl.textContent);
+// const itemEl = Document.querySelector('.item');
+const categoriesEl = document.querySelector('#categories');
+const childrenCountEl = categoriesEl.children.length;
+console.log('Number of categories:', childrenCountEl);
+
+
+

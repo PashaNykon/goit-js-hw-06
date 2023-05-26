ca87aa9896c53f01b99f2df87cdbaa1936e18bcc
PashaNykon <pavlonykon@ukr.net>
2023-05-26 20:05:26 +0300

    8-9 дз

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (mail === '' || password === '') {
+        alert('Всі поля повинні бути заповнені');
+    }
+
+    const formData = {
+        mail,
+        password,
+    }
+    
+    console.log(formData);
+    formEl.reset();
+       
+}
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeBgColor = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+const backgroundColor = document.querySelector('body');
+
+btnChangeBgColor.addEventListener('click', changeColor)
+
+function changeColor() {
+  spanEl.textContent = getRandomHexColor();
+  backgroundColor.style.backgroundColor = getRandomHexColor();
+}
